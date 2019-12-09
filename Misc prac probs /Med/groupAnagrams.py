97efa5d8fa0269712b34cf157274ea786385e4f4
Paul Bass <bass_p1@denison.edu>
2019-12-09 10:37:32 -0500

    added groupAnagrams

--- Misc prac probs /Med/groupAnagrams.py
+++ Misc prac probs /Med/groupAnagrams.py
@@ -16,8 +16,25 @@ Note:
     All inputs will be in lowercase.
     The order of your output does not matter.
 """
+def createAnaList():
+  ret = []
+  for i in range(0,26):
+    ret.append(0)
+  return ret
+
 def groupAna(anagramList):
-    for ana in anagramList:
-        wordHash = 
-        for letter in ana:
-          
+  anaSet = {}
+  retArr = []
+  currSlot = 0
+  for ana in anagramList:
+    anaZeroes = createAnaList()
+    for letter in ana:
+      print(anaZeroes[ord(letter)-97])
+      anaZeroes[ord(letter)-97] = anaZeroes[ord(letter)-97]+1
+    if str(anaZeroes) not in anaSet.keys():
+      anaSet[str(anaZeroes)] = currSlot
+      currSlot = currSlot+1
+      retArr.append([])
+    retArr[anaSet[str(anaZeroes)]].append(ana)
+  return retArr
+print(groupAna(["eat", "tea", "tan", "ate", "nat", "bat"]))
