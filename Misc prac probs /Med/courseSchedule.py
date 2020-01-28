3ad84d1f41f4bd422ddbc5c7c3c283c013d1e291
Paul Bass <bass_p1@denison.edu>
2020-01-27 22:13:22 -0500

    added course schedule

--- Misc prac probs /Med/courseSchedule.py
+++ Misc prac probs /Med/courseSchedule.py
@@ -1,15 +1,28 @@
 """
-There are a total of n courses you have to take, labeled from 0 to n-1.
+There are a total of numCourses prerequisites you have to take, labeled from 0 to numCourses-1.
 
-Some courses may have prerequisites, for example to take course 0 you have to
+Some prerequisites may have prerequisites, for example to take course 0 you have to
 first take course 1, which is expressed as a pair: [0,1]
 
-Given the total number of courses and a list of prerequisite pairs, is it
-possible for you to finish all courses?
+Given the total number of prerequisites and a list of prerequisite pairs, is it
+possible for you to finish all prerequisites?
 """
 
-def findSchedule(numSlots,courses):
-    visited = []
-    for course in courses:
-        visited.append(False)
-    
+def findSchedule(numCourses,prerequisites):
+    adjMatrix = {}
+    for course in prerequisites:
+        if course[0] in adjMatrix.keys():
+            adjMatrix[course[0]].append(course[1])
+        else:
+            adjMatrix[course[0]] = [course[1]]
+    queue = [prerequisites[0]]
+    seenCourses = set(prerequisites[0][0])
+    while queue:
+        currLoc = queue.pop()
+        if currLoc in seenCourses:
+            return False
+        seenCourses.add(currLoc)
+        for neighbor in adjMatrix[currLoc]:
+            queue.append(neighbor)
+    return True
+        
